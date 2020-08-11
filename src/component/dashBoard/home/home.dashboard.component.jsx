05fe90cb0387d6505bc08760e7c3672180d9a42f
endiesworld <endiesworld@gmail.com>
2020-08-11 13:17:37 +0100

    Added the legends for the inventory piechart

--- src/component/dashBoard/home/home.dashboard.component.jsx
+++ src/component/dashBoard/home/home.dashboard.component.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import {HomeParentDiv} from './home.dashboard.styled' ;
-import DisplayBoard from "./displayBoard.component" ;
-import {BusinessElement } from "../dashBoardTools" ;
-
+import React from "react";
+import { HomeParentDiv } from "./home.dashboard.styled";
+import DisplayBoard from "./displayBoard.component";
+import { BusinessElement } from "../dashBoardTools";
 
 function HomeDashboard() {
-    
-    const homeScreenProperies = BusinessElement.filter((property) => property.id !== "home") ;
-    return (
-        <HomeParentDiv >
-            { homeScreenProperies.map((element) => 
-            <DisplayBoard key = {element.id} display ={element}/> )
-            
-            }
-        </HomeParentDiv>
-    )
+  const homeScreenProperies = BusinessElement.filter(
+    (property) => property.id !== "home"
+  );
+  return (
+    <HomeParentDiv>
+      {homeScreenProperies.map((element) => (
+        <DisplayBoard key={element.id} display={element} />
+      ))}
+    </HomeParentDiv>
+  );
 }
 
-export default HomeDashboard
+export default HomeDashboard;
