d4ffbd2e61b5fd236ecd91c959481a82a8881888
endiesworld <endiesworld@gmail.com>
2020-11-15 05:24:12 +0100

    Updates

---
+++ precache-manifest.8ddd864775baef256c134daa837b1747.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b853c23da6734ccded0800682380335e",
+    "url": "/bar-x/index.html"
+  },
+  {
+    "revision": "6964da3586d4425531cc",
+    "url": "/bar-x/static/css/main.a07141c5.chunk.css"
+  },
+  {
+    "revision": "753725af38e407f3e14e",
+    "url": "/bar-x/static/js/2.b56a37a5.chunk.js"
+  },
+  {
+    "revision": "5d091aea7ed00ab43a92de6446f31ff3",
+    "url": "/bar-x/static/js/2.b56a37a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6964da3586d4425531cc",
+    "url": "/bar-x/static/js/main.240d4a8e.chunk.js"
+  },
+  {
+    "revision": "2038408bd8e6cb8077c7",
+    "url": "/bar-x/static/js/runtime-main.1543f291.js"
+  },
+  {
+    "revision": "24a141dec837a6720a79dddf29a5bed6",
+    "url": "/bar-x/static/media/Check-sign.24a141de.svg"
+  },
+  {
+    "revision": "a6068d3344d24441749b961db5e418b6",
+    "url": "/bar-x/static/media/add-itemsAndMenu.a6068d33.svg"
+  },
+  {
+    "revision": "b708a2ec2cbc47ba2f418a34b88ae350",
+    "url": "/bar-x/static/media/beer-mug.b708a2ec.svg"
+  },
+  {
+    "revision": "5cf3470da6baa1433b7161bbc05db248",
+    "url": "/bar-x/static/media/business-owner.5cf3470d.svg"
+  },
+  {
+    "revision": "9711efaa201b6f77fd7ced354f460fed",
+    "url": "/bar-x/static/media/business-performance.9711efaa.svg"
+  },
+  {
+    "revision": "d669aa92f93e1f8e9a8e0134cbb40639",
+    "url": "/bar-x/static/media/cancel.d669aa92.svg"
+  },
+  {
+    "revision": "888c2de9d4f7e89e84ca616a23fc40c5",
+    "url": "/bar-x/static/media/cart-arrow-down-solid.888c2de9.svg"
+  },
+  {
+    "revision": "e66809b14aed615018c699b80eff5fcf",
+    "url": "/bar-x/static/media/cocktail-solid.e66809b1.svg"
+  },
+  {
+    "revision": "3ecb021eed8f83c352b38c5a43a4b979",
+    "url": "/bar-x/static/media/copyright.3ecb021e.svg"
+  },
+  {
+    "revision": "41f448a77f83983214960de6ad783cbf",
+    "url": "/bar-x/static/media/customers-review.41f448a7.svg"
+  },
+  {
+    "revision": "71d0aa10d287b732e7e612498550707a",
+    "url": "/bar-x/static/media/drink-beer-jar.71d0aa10.svg"
+  },
+  {
+    "revision": "aec66c882a2ecfee6ab002db23f51f51",
+    "url": "/bar-x/static/media/employee-performance.aec66c88.svg"
+  },
+  {
+    "revision": "fd72ba90406b8974c9b9b8f49170eae8",
+    "url": "/bar-x/static/media/home.iventory.fd72ba90.svg"
+  },
+  {
+    "revision": "a38a5c46f9c1331e05aadcb43c19ddc6",
+    "url": "/bar-x/static/media/incidence-damages.a38a5c46.svg"
+  },
+  {
+    "revision": "80f5c8af447a73d5d8043aa66697dedb",
+    "url": "/bar-x/static/media/maintenance.80f5c8af.svg"
+  },
+  {
+    "revision": "abaa872b23c3fad432586b4c95004a7e",
+    "url": "/bar-x/static/media/management-group.abaa872b.svg"
+  },
+  {
+    "revision": "d2fadb850edeaf06ac4d2f3bdf9d97f8",
+    "url": "/bar-x/static/media/menu-list.d2fadb85.svg"
+  },
+  {
+    "revision": "c4fe5c75a24363820526fee39ed245a9",
+    "url": "/bar-x/static/media/merry-men.c4fe5c75.svg"
+  },
+  {
+    "revision": "e6fc30ba7781c7432f3459f4dfe02436",
+    "url": "/bar-x/static/media/navbar-barx.e6fc30ba.svg"
+  },
+  {
+    "revision": "1d56f306f847ed825e3f62239ff6d4c1",
+    "url": "/bar-x/static/media/page-barx.1d56f306.svg"
+  },
+  {
+    "revision": "6675eeb22e0d919502a2bf0b76974c4c",
+    "url": "/bar-x/static/media/purchase-order.6675eeb2.svg"
+  },
+  {
+    "revision": "0541a54bd12e5bd6e2196474924dd1d4",
+    "url": "/bar-x/static/media/refrigerator-slide.0541a54b.svg"
+  },
+  {
+    "revision": "0373e42611683d01f63600fa9fd14515",
+    "url": "/bar-x/static/media/restaurant.0373e426.svg"
+  },
+  {
+    "revision": "39951701922ad847a67c87467c6a884e",
+    "url": "/bar-x/static/media/revenue-expenses.39951701.svg"
+  },
+  {
+    "revision": "5a812ad746cf46bcd9f29772b2787052",
+    "url": "/bar-x/static/media/sales-report.5a812ad7.svg"
+  },
+  {
+    "revision": "aea8f843394ba6cb49e2c87f8382145a",
+    "url": "/bar-x/static/media/waiters.aea8f843.svg"
+  }
+]);
